50808c8029299dd8e032165f0743f5844aa1dc88
chasegnil <gnil_hmz@126.com>
2019-06-29 19:39:56 +0800

    修改测试类

--- spring-boot-project-example-mail/src/test/java/MailUtilTest.java
+++ spring-boot-project-example-mail/src/test/java/MailUtilTest.java
@@ -1,5 +1,6 @@
 import com.chasegnil.Application;
 import com.chasegnil.mail.MailUtil;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,20 @@ public class MailUtilTest {
     @Autowired
     private MailUtil mailUtil;
 
+    // TODO 设置发件人
+    private String deliver = "xxx@xxx.com";
+    private Set<String> receivers = new HashSet<>();
+    private Set<String> carbonCopys = new HashSet<>();
+
+    @Before
+    public void initReceiversAndCarbonCopys(){
+        // TODO 设置收件人和抄送人
+        receivers.add("xxxx@xxx.com");
+        carbonCopys.add("xxxxx@xxx.com");
+    }
+
     @Test
     public void sendSimpleMail(){
-        String deliver = "gnil_hmz@126.com";
-        Set<String> receivers = new HashSet<>();
-        receivers.add("386198517@qq.com");
-        Set<String> carbonCopys = new HashSet<>();
-        carbonCopys.add("gnil_hmz@126.com");
         String subject = "This is a simple email";
         String content = "This is a simple content";
         mailUtil.sendSimpleMail(deliver, receivers, carbonCopys, subject, content);
@@ -34,11 +42,6 @@ public class MailUtilTest {
 
     @Test
     public void sendHtmlMail(){
-        String deliver = "gnil_hmz@126.com";
-        Set<String> receivers = new HashSet<>();
-        receivers.add("386198517@qq.com");
-        Set<String> carbonCopys = new HashSet<>();
-        carbonCopys.add("gnil_hmz@126.com");
         String subject = "This is a Html Style email";
         String content = "<h1> This is a Html Style content mail </h1>";
         mailUtil.sendHtmlMail(deliver, receivers, carbonCopys, subject, content, true);
@@ -46,11 +49,6 @@ public class MailUtilTest {
 
     @Test
     public void sendAttachmentFileMail(){
-        String deliver = "gnil_hmz@126.com";
-        Set<String> receivers = new HashSet<>();
-        receivers.add("386198517@qq.com");
-        Set<String> carbonCopys = new HashSet<>();
-        carbonCopys.add("gnil_hmz@126.com");
         String subject = "This is an attachment file email";
         String content = "<h1> This is an attachment file email </h1>";
         File file = new File("E:\\opt\\netty\\netty-demo-telnet\\logs\\info.log");
@@ -60,11 +58,6 @@ public class MailUtilTest {
 
     @Test
     public void sendTemplateEmail() {
-        String deliver = "gnil_hmz@126.com";
-        Set<String> receivers = new HashSet<>();
-        receivers.add("386198517@qq.com");
-        Set<String> carbonCopys = new HashSet<>();
-        carbonCopys.add("gnil_hmz@126.com");
         String template = "mail/InternalServerErrorTemplate";
         String subject = "This is a template email";
         Context context = new Context();
